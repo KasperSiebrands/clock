d65ee3f67f2b7bed5a49e2329092cbcbce995fdd
kaspersiebrands <kasper.siebrands@gmail.com>
2024-09-22 12:08:15 +0200

    added 2 smaller cickles, one for seconds and one for miliseconds

--- analogClock.py
+++ analogClock.py
@@ -7,21 +7,27 @@ pygame.init()
 #set up display
 screen = pygame.display.set_mode((800, 800))   #800,800 is pixel size of the window
 pygame.display.set_caption("Analog Watch by Kasper Siebrands") # added a name for my watch
-screen.fill((255, 255, 255)) # Fill the screen with white
+screen.fill((255, 255, 255)) #backgroundcolor white...
 
 big_clock_color = (0,0,0)
 big_clock_position = (400, 400)
 big_clock_radius = 380
-big_clock_widht = 3
+big_clock_widht = 5
 
 small_clock_color = (0,0,0)
-small_clock_position = (400, 590)
-small_clock_radius = 150
-small_clock_width = 2
+small_clock_position_left = (250, 525)
+small_clock_position_right = (550, 525)
+small_clock_radius = 100
+small_clock_width = 3
 
-#setup clock
+#setup clock big
 pygame.draw.circle(screen, big_clock_color, big_clock_position, big_clock_radius, big_clock_widht) #where to draw, colour, placement, radius, width
-pygame.draw.circle(screen, small_clock_color, small_clock_position, small_clock_radius, small_clock_width) #where to draw, colour, placement, radius, width
+
+#setup clock small left
+pygame.draw.circle(screen, small_clock_color, small_clock_position_left, small_clock_radius, small_clock_width) #where to draw, colour, placement, radius, width
+#setup clock small right
+pygame.draw.circle(screen, small_clock_color, small_clock_position_right, small_clock_radius, small_clock_width) #where to draw, colour, placement, radius, width
+
 
 # Make sure the window stays open until the user closes it copied from draw.py
 run_flag = True
