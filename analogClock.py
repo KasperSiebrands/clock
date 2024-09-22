fbe020370c03b0c09deb3c7050631a6831258fca
kaspersiebrands <kasper.siebrands@gmail.com>
2024-09-22 13:15:24 +0200

    added minutes and hours hand

--- analogClock.py
+++ analogClock.py
@@ -18,17 +18,27 @@ big_clock_color = (0,0,0)
 big_clock_position = (400, 400)
 big_clock_radius = 380
 big_clock_widht = 5
+length_big_clock_minutes = 360
+length_big_clock_hours = 300
 
 small_clock_color = (0,0,0)
 small_clock_position_left = (250, 525)
 small_clock_position_right = (550, 525)
 small_clock_radius = 100
 small_clock_width = 3
+length_small_clock_seconds = 90  # Length of the second
 
 line_color_seconds = (0,0,0)
 line_width_seconds = 3
-x_center_small_clock_left, y_center_small_clock_left = 250, 525
-x_center_small_clock_right, y_center_small_clock_right = 550, 525
+line_width_minutes = 3
+line_width_hours = 5
+
+
+x_center_small_clock_left, y_center_small_clock_left = small_clock_position_left
+x_center_small_clock_right, y_center_small_clock_right = small_clock_position_right
+x_center_big_clock_minutes, y_center_big_clock_minutes = big_clock_position[0], big_clock_position[1]
+x_center_big_clock_hours, y_center_big_clock_hours = big_clock_position[0], big_clock_position[1]
+
 
 
 
@@ -39,7 +49,6 @@ while run_flag is True:
 
     #setup clock big
     pygame.draw.circle(screen, big_clock_color, big_clock_position, big_clock_radius, big_clock_widht) #where to draw, colour, placement, radius, width
-
     #setup clock small left
     pygame.draw.circle(screen, small_clock_color, small_clock_position_left, small_clock_radius, small_clock_width) #where to draw, colour, placement, radius, width
     #setup clock small right
@@ -47,24 +56,40 @@ while run_flag is True:
 
 
     now = datetime.now()
+    hours = now.hour % 12 #12 is needed to make it a 12 hour clock, otherwise it wont be like an analog clock...
+    minutes = now.minute
     seconds = now.second
     milliseconds = now.microsecond // 1000
     
-    angle_seconds = math.radians(seconds * 6)
+    #made a mistake earlier with calucation of miliseconds so now with explanation...
+    angle_hours = math.radians(hours * 30)  # 360 degrees / 12 hours = 30 degrees per hour
+    angle_minutes = math.radians(minutes * 6)  # 360 degrees for whole circle / 60 minutes = 6 degrees per minute
+    angle_seconds = math.radians(seconds * 6) # 360 degrees / 60 seconds = 6 degrees per second
+    angle_milliseconds = math.radians(milliseconds * 0.36) # 360 degrees / 1000 milliseconds = 0.36 degrees per millisecond
+
+    # Calculate the end position of the hours for big circle
+    x_end_big_clock_hours = x_center_big_clock_hours + length_big_clock_hours * math.sin(angle_hours) #calculate the end of the line
+    y_end_bog_clock_hours = y_center_big_clock_hours - length_big_clock_hours * math.cos(angle_hours) #calculate the end of the line
     
-    angle_milliseconds = math.radians(milliseconds * 0.36)
+    # Calculate the end position of the minutes for big circle
+    x_end_big_clock_minutes = x_center_big_clock_minutes + length_big_clock_minutes * math.sin(angle_minutes) #calculate the end of the line
+    y_end_bog_clock_minutes = y_center_big_clock_minutes - length_big_clock_minutes * math.cos(angle_minutes) #calculate the end of the line
     
     # Calculate the end position of the second for left circle
-    length_small_clock = 90  # Length of the second
-    x_end_small_clock_left = x_center_small_clock_left + length_small_clock * math.sin(angle_seconds) #calculate the end of the line
-    y_end_small_clock_left = y_center_small_clock_left - length_small_clock * math.cos(angle_seconds) #calculate the end of the line
+    x_end_small_clock_left = x_center_small_clock_left + length_small_clock_seconds * math.sin(angle_seconds) #calculate the end of the line
+    y_end_small_clock_left = y_center_small_clock_left - length_small_clock_seconds * math.cos(angle_seconds) #calculate the end of the line
 
-    x_end_small_clock_right = x_center_small_clock_right + length_small_clock * math.sin(angle_milliseconds) #calculate the end of the line
-    y_end_small_clock_right = y_center_small_clock_right - length_small_clock * math.cos(angle_milliseconds) #calculate the end of the line
+   # Calculate the end position of the second for right circle
+    x_end_small_clock_right = x_center_small_clock_right + length_small_clock_seconds * math.sin(angle_milliseconds) #calculate the end of the line
+    y_end_small_clock_right = y_center_small_clock_right - length_small_clock_seconds * math.cos(angle_milliseconds) #calculate the end of the line
 
+
+    # Draw the hours big clock
+    pygame.draw.line(screen, line_color_seconds, (x_center_big_clock_hours, y_center_big_clock_hours), (x_end_big_clock_hours, y_end_bog_clock_hours), line_width_hours)  #aaline is more soft around edges...
+    # Draw the minutes big clock
+    pygame.draw.line(screen, line_color_seconds, (x_center_big_clock_minutes, y_center_big_clock_minutes), (x_end_big_clock_minutes, y_end_bog_clock_minutes), line_width_minutes)  #aaline is more soft around edges...
     # Draw the second hand left
     pygame.draw.line(screen, line_color_seconds, (x_center_small_clock_left, y_center_small_clock_left), (x_end_small_clock_left, y_end_small_clock_left), line_width_seconds)  #aaline is more soft around edges...
-
     # Draw the second hand right
     pygame.draw.line(screen, line_color_seconds, (x_center_small_clock_right, y_center_small_clock_right), (x_end_small_clock_right, y_end_small_clock_right), line_width_seconds)  #aaline is more soft around edges...
 
@@ -78,6 +103,6 @@ while run_flag is True:
     pygame.display.flip()  # Refresh the screen so drawing appears
 
     # Limit the framerate to 24 FPS
-    clock.tick(24)
+    clock.tick(25)
 
 pygame.quit()
